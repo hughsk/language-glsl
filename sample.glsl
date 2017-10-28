0344f7d5bd72c25e5e02063b5a0a7cde2ce5c406
Ford Hurley <ford.hurley@gmail.com>
2017-10-28 11:33:47 -0400

    Reduce duplication by matching character ranges

--- sample.glsl
+++ sample.glsl
@@ -1,19 +1,23 @@
 #ifndef HELLO_WORLD
-#define HELLO_WORLD vec3(0.0, 0.0, 0.0)
+  #define HELLO_WORLD vec3(0.0, 0.0, 0.0)
 #else
-#define LOREM_IPSUM
+  #define LOREM_IPSUM
 #endif
 
 #pragma GENERIC_PRAGMA
 
 #pragma glslify: noise = require(glsl-noise)
 #pragma glslify: random = require(glsl-random)
-#pragma glslify: export(x)
+#pragma glslify: export(x)s
 
 void main() {
-  vec3 a = noise(gl_FragColor.xy);
-  vec3 b = y.xyz;
+  vec3 a = noise(gl_FragCoord.xy);
+  vec4 b = a.zyxx;
   ivec4 c = ivec4(0);
+  mat2 d = mat2(1);
+  mat3 e = mat3(a, b, vec3(2));
 
-  a.xyz = gl_FragColor.xyz;
+  a.xyz = gl_FragCoord.xyz;
+
+  gl_FragColor = vec4(a.rgb, b.a);
 }
